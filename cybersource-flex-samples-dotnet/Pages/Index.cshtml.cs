607b8583a941956889a893bc46e0a526334dec23
brianmc <bmgtech@gmail.com>
2019-04-10 15:14:12 -0700

    initial push

---
+++ cybersource-flex-samples-dotnet/Pages/Index.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CyberSource.Api;
+using CyberSource.Model;
+
+namespace cybersource_flex_samples_dotnet.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string jwkJSON { get; set; }
+
+        public void OnGet()
+        {
+            // Call the .NET CyberSource SDK to generate the Flex Key
+
+            jwkJSON = "{\"kid\":\"HKJHKJ\"}";
+
+            var requestObj = new GeneratePublicKeyRequest("RsaOaep256", "http://localhost");
+
+            try
+            {
+                var configDictionary = new Configuration().GetConfiguration();
+                var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
+                var apiInstance = new KeyGenerationApi(clientConfig);
+
+                var result = apiInstance.GeneratePublicKey(requestObj);
+                Console.WriteLine(result);
+                jwkJSON = result.Jwk.ToJson();
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception on calling the API: " + e.Message);
+                return;
+            }
+        }
+    }
+
+    public class Configuration
+    {
+        // initialize dictionary object
+        private readonly Dictionary<string, string> _configurationDictionary = new Dictionary<string, string>();
+
+        public Dictionary<string, string> GetConfiguration()
+        {
+            _configurationDictionary.Add("authenticationType", "HTTP_SIGNATURE");
+            _configurationDictionary.Add("merchantID", "testrest");
+            _configurationDictionary.Add("merchantsecretKey", "yBJxy6LjM2TmcPGu+GaJrHtkke25fPpUX+UY6/L/1tE=");
+            _configurationDictionary.Add("merchantKeyId", "08c94330-f618-42a3-b09d-e1e43be5efda");
+            _configurationDictionary.Add("keysDirectory", "Resource");
+            _configurationDictionary.Add("keyFilename", "testrest");
+            _configurationDictionary.Add("runEnvironment", "cybersource.environment.sandbox");
+            _configurationDictionary.Add("keyAlias", "testrest");
+            _configurationDictionary.Add("keyPass", "testrest");
+            _configurationDictionary.Add("enableLog", "FALSE");
+            _configurationDictionary.Add("logDirectory", string.Empty);
+            _configurationDictionary.Add("logFileName", string.Empty);
+            _configurationDictionary.Add("logFileMaxSize", "5242880");
+            _configurationDictionary.Add("timeout", "1000");
+            _configurationDictionary.Add("proxyAddress", string.Empty);
+            _configurationDictionary.Add("proxyPort", string.Empty);
+
+            return _configurationDictionary;
+        }
+    }
+}
